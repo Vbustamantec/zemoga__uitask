74cf12583d53bf7a2f814c560f5aad464c2962fd
Victor Bustamante Cera <victorbust.c@gmail.com>
2022-06-29 18:55:51 -0400

    improving accesibility
    improving accesibility

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,12 @@ const Navbar = () => {
     <nav className="nav" role="navigation">
       <div className="max-centered">
         <h1 className="nav__logo">Rule of thumb.</h1>
-        <button type="button" className="nav__hamburger icon-button" alt="Open Menu">
+        <button
+          type="button"
+          className="nav__hamburger icon-button"
+          alt="Open Menu"
+          aria-label="hamburger menu"
+        >
           <svg width="25" height="20" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 0h25v4H0V0zm0 8h25v4H0V8zm0 8h25v4H0v-4z" fill="#FFF" fillRule="nonzero" />
           </svg>
